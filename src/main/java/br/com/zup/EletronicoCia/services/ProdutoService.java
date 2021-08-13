8bce574afff6881c172c318de2ebf545e4b18352
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-13 15:18:20 -0300

    subindo produto service

--- src/main/java/br/com/zup/EletronicoCia/services/ProdutoService.java
+++ src/main/java/br/com/zup/EletronicoCia/services/ProdutoService.java
@@ -1,4 +1,40 @@
 package br.com.zup.EletronicoCia.services;
 
-public class ProdutoService {
+import br.com.zup.EletronicoCia.dtos.ProdutoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService{
+     private List<ProdutoDto>listaProduto = new ArrayList<>();
+    public void cadastrar(ProdutoDto produtoDto){
+        verificasSeExisteProdutoIgual(produtoDto);
+        listaProduto.add(produtoDto);
+    }
+    public List<ProdutoDto>listar(){
+        return listaProduto;
+    }
+
+    public void verificasSeExisteProdutoIgual(ProdutoDto produtoDto){
+        for (ProdutoDto produto:listaProduto) {
+            if (produto.getNomeDoProduto().equals(produtoDto.getNomeDoProduto())){
+                throw new RuntimeException("PRODUTO JA EXISTENTE");
+            }
+        }
+    }
+    public ProdutoDto localizarPorNome(String nome) {
+        for (ProdutoDto produto:listaProduto) {
+            if (produto.getNomeDoProduto().equals(nome)){
+                return produto;
+            }
+
+        }
+        throw new RuntimeException("PRODUTO NÃO ENCONTRADO");
+    }
+
 }
+
+
+
