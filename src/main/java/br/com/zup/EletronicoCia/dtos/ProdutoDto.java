81c8b6bd6163490cb04b0137963e9c64b8334746
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-13 15:14:33 -0300

    subindo produto Dto

--- src/main/java/br/com/zup/EletronicoCia/dtos/ProdutoDto.java
+++ src/main/java/br/com/zup/EletronicoCia/dtos/ProdutoDto.java
@@ -1,69 +1,65 @@
 package br.com.zup.EletronicoCia.dtos;
 
+
 import br.com.zup.EletronicoCia.enuns.Categoria;
 
-import java.math.BigDecimal;
-
 public class ProdutoDto {
-
-    public class Produto {
-        public String nomeDoProduto;
-        private int idProduto;
+        private String nomeDoProduto;
         private String descricaoDoProduto;
-        private BigDecimal valorDoProduto;
+        private double valorDoProduto;
         private int quantidade;
+        private Categoria categoria;
+        public ProdutoDto() {
 
-
-        public Produto(String nomeDoProduto, int idProduto, String descricaoDoProduto, BigDecimal valorDoProduto, int quantidade) {
-            this.nomeDoProduto = nomeDoProduto;
-            this.idProduto = idProduto;
-            this.descricaoDoProduto = descricaoDoProduto;
-            this.valorDoProduto = valorDoProduto;
-            this.quantidade = quantidade;
         }
 
-        public String getNomeDoProduto() {
-            return nomeDoProduto;
-        }
-
-        public void setNomeDoProduto(String nomeDoProduto) {
-            this.nomeDoProduto = nomeDoProduto;
-        }
-
-        public int getIdProduto() {
-            return idProduto;
-        }
-
-        public void setIdProduto(int idProduto) {
-            this.idProduto = idProduto;
-        }
-
-        public String getDescricaoDoProduto() {
-            return descricaoDoProduto;
-        }
-
-        public void setDescricaoDoProduto(String descricaoDoProduto) {
-            this.descricaoDoProduto = descricaoDoProduto;
-        }
-
-        public BigDecimal getValorDoProduto() {
-            return valorDoProduto;
-        }
-
-        public void setValorDoProduto(BigDecimal valorDoProduto) {
-            this.valorDoProduto = valorDoProduto;
-        }
-
-        public int getQuantidade() {
-            return quantidade;
-        }
-
-        public void setQuantidade(int quantidade) {
-            this.quantidade = quantidade;
-        }
-
+    public ProdutoDto(String nomeDoProduto, String descricaoDoProduto, double valorDoProduto, int quantidade, Categoria categoria) {
+        this.nomeDoProduto = nomeDoProduto;
+        this.descricaoDoProduto = descricaoDoProduto;
+        this.valorDoProduto = valorDoProduto;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
     }
 
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    public String getDescricaoDoProduto() {
+        return descricaoDoProduto;
+    }
+
+    public void setDescricaoDoProduto(String descricaoDoProduto) {
+        this.descricaoDoProduto = descricaoDoProduto;
+    }
+
+    public double getValorDoProduto() {
+        return valorDoProduto;
+    }
+
+    public void setValorDoProduto(double valorDoProduto) {
+        this.valorDoProduto = valorDoProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
 
 
