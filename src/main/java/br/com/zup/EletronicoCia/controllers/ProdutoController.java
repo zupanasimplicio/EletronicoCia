c25f6431087887c16fe2a745032155b471b09d3d
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-13 15:12:36 -0300

    subindo produto controller com as funcinalidades de cadastrar e listar produto

--- src/main/java/br/com/zup/EletronicoCia/controllers/ProdutoController.java
+++ src/main/java/br/com/zup/EletronicoCia/controllers/ProdutoController.java
@@ -1,4 +1,32 @@
 package br.com.zup.EletronicoCia.controllers;
 
-public class ProdutoController {
-}
+import br.com.zup.EletronicoCia.dtos.ProdutoDto;
+import br.com.zup.EletronicoCia.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/produtos")
+    public class ProdutoController{
+    @Autowired
+    ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity cadastrar (@Valid @RequestBody ProdutoDto produtoDto) {
+        produtoService.cadastrar(produtoDto);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping
+    public ResponseEntity<List<ProdutoDto>>lista() {
+        return ResponseEntity.ok(produtoService.listar());
+    }
+
+
+    }
+
+
